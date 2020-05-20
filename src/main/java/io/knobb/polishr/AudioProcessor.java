c2e0dd8c374874494d8bd4072d5d8855e2d35679
Jeff <jeffreysapia@gmail.com>
2020-05-20 00:03:20 -0400

    WIP: Refactor code for generating wave data

--- src/main/java/io/knobb/polishr/AudioProcessor.java
+++ src/main/java/io/knobb/polishr/AudioProcessor.java
@@ -5,7 +5,16 @@ import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.transform.DftNormalization;
+import org.apache.commons.math3.transform.FastFourierTransformer;
+import org.apache.commons.math3.transform.TransformType;
+import org.apache.commons.math3.transform.TransformUtils;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AudioProcessor extends Application {
 
@@ -14,37 +23,70 @@ public class AudioProcessor extends Application {
     }
 
     @Override
-    public void start(Stage window) throws Exception {
-        final NumberAxis axisX = new NumberAxis();
-        final NumberAxis axisY = new NumberAxis();
-        final LineChart<Number,Number> lineChart = new LineChart<>(axisX, axisY);
-        lineChart.setTitle("Sine Wave Example");
-        lineChart.setCreateSymbols(false);
-        lineChart.getData().add(createSineWave());
-        Scene scene = new Scene(lineChart, 700, 400);
+    public void start(Stage window) {
+        final VBox layout = new VBox();
+        final LineChart<Number,Number> timeData = new LineChart<>(new NumberAxis(), new NumberAxis());
+        final LineChart<Number,Number> fftData = new LineChart<>(new NumberAxis(), new NumberAxis());
+
+        timeData.setTitle("Time Data");
+        timeData.setCreateSymbols(false);
+        timeData.setLegendVisible(false);
+        fftData.setTitle("Fourier Transform");
+        fftData.setCreateSymbols(false);
+        fftData.setLegendVisible(false);
+
+        layout.getChildren().add(timeData);
+        layout.getChildren().add(fftData);
+
+        double[] waveData = generateWaveData(1024);
+
+        timeData.getData().add(createSineWave(waveData));
+        fftData.getData().add(createFFTSpectrum(waveData));
+
+        Scene scene = new Scene(layout, 900, 600);
         window.setScene(scene);
         window.setTitle("Fast Fourier Transform");
         window.show();
     }
 
-    private XYChart.Series createSineWave() {
-        double[][] data = new double[512][2];
-        double radian;
-        double frequencyFactor = 20;
-        double amplitudeFactor = 0.5;
-        XYChart.Series series = new XYChart.Series();
-        series.setName("Sine Wave");
+    private double[] generateWaveData(int fftSize) {
+        double[] data = new double[fftSize];
+        double radians;
+        double frequencyFactor = 1;
+        double amplitudeFactor = 1;
         for (int i=0;i<data.length;i++) {
-            data[i][0] = i;
-            // radian = (2*i) / (2*Math.PI);
-            radian = Math.toRadians(frequencyFactor*i);
-            data[i][1] = (amplitudeFactor*(Math.sin(radian)));
-            radian++;
-            series.getData().add(new XYChart.Data((int)data[i][0], data[i][1]));
+            data[i] = i;
+            // Convert frequency selection to radians to generate sine wave
+            radians = Math.toRadians( frequencyFactor * i );
+            // Apply amplitude value to generated sine wave data
+            data[i] = ( amplitudeFactor * ( Math.sin( radians ) ) );
         }
-//        Complex[] dataVector = TransformUtils.createComplexArray(data);
-//        FastFourierTransformer transformer = new FastFourierTransformer(DftNormalization.STANDARD);
-//        Complex[] frequencyData = transformer.transform(dataVector, TransformType.FORWARD);
+        return data;
+    }
+
+    private XYChart.Series createSineWave(double[] data) {
+        XYChart.Series series = new XYChart.Series();
+        for (int i=0;i<data.length;i++) {
+            series.getData().add(new XYChart.Data( i, data[i]));
+        }
         return series;
+    }
+
+    private XYChart.Series createFFTSpectrum(double[] data) {
+//      Complex[] dataVector = TransformUtils.createComplexArray(data);
+        FastFourierTransformer transformer = new FastFourierTransformer(DftNormalization.STANDARD);
+        Complex[] frequencyData = transformer.transform(data, TransformType.FORWARD);
+
+//      double[][] outputData = TransformUtils.createRealImaginaryArray(frequencyData);
+
+        double[] outputData = new double[data.length];
+        AtomicReference<Integer> count = new AtomicReference<>(0);
+        Arrays.stream(frequencyData).forEach((node) -> {
+            int index = count.get();
+            System.out.println("Index:" + index + " Real:" + node.getReal());
+            outputData[index] = node.getReal();
+            count.set(index + 1);
+        });
+        return createSineWave(outputData);
     }
 }
