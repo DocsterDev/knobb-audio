1e6e2c19726045b57d0693e87f76ff737b764e1f
Jeff <jeff@artfx.io>
2020-11-21 19:10:32 -0500

    Working FFT and Visualizer

--- src/main/java/io/knobb/polishr/contoller/MainController.java
+++ src/main/java/io/knobb/polishr/contoller/MainController.java
@@ -1,48 +1,32 @@
 package io.knobb.polishr.contoller;
 
 import io.knobb.polishr.filter.ExponentialMovingAverageFilter;
-import io.knobb.polishr.filter.SuperSmootherFilter;
 import io.knobb.polishr.io.MP3DecoderModded;
 import io.knobb.polishr.player.AudioPlayer;
+import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.AreaChart;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.shape.Circle;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 import org.apache.commons.math3.complex.Complex;
 import org.apache.commons.math3.transform.DftNormalization;
 import org.apache.commons.math3.transform.FastFourierTransformer;
 import org.apache.commons.math3.transform.TransformType;
 
-import java.io.FileInputStream;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class MainController implements Initializable {
 
     public StackPane playButton;
-    public Circle visualizerLow;
-    public Circle visualizerMid;
-    public Circle visualizerHigh;
 
     @FXML
-    public Rectangle bar0;
-    @FXML
-    public Rectangle bar1;
-    @FXML
-    public Rectangle bar2;
-    @FXML
-    public Rectangle bar3;
-    @FXML
-    public Rectangle bar4;
+    public HBox visualizer;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -51,7 +35,6 @@ public class MainController implements Initializable {
 
     public void handleButtonClick () {
         System.out.println("Button Clicked");
-
         Task<Void> audio = new Task<Void>() {
             @Override
             protected Void call() throws Exception {
@@ -64,7 +47,32 @@ public class MainController implements Initializable {
     }
 
     private void process() {
-        System.out.println("Playing audio");
+        int numBars = 40;
+        List<String> colors = new ArrayList<>();
+        colors.add("7ae2ff");
+        colors.add("4bd8ff");
+        colors.add("2eccf4");
+        colors.add("1bd1ff");
+        Map<Integer, ExponentialMovingAverageFilter> maFilter = new HashMap<>();
+        Platform.runLater(new Runnable(){
+            @Override
+            public void run() {
+                for (int i = 0; i < numBars; i++) {
+                    Collections.shuffle(colors);
+                    maFilter.put(i, new ExponentialMovingAverageFilter(10));
+                    Rectangle rectangle = new Rectangle();
+                    rectangle.setId("bar" + i);
+                    rectangle.setFill(Paint.valueOf(colors.get(0)));
+                    rectangle.setWidth(10);
+                    rectangle.setHeight(1);
+                    rectangle.setStrokeType(StrokeType.INSIDE);
+                    rectangle.setLayoutX(135);
+                    rectangle.setLayoutY(10);
+                    rectangle.setSmooth(false);
+                    visualizer.getChildren().add(rectangle);
+                }
+            }
+        });
         try {
             //WaveDecoder waveDecoder = new WaveDecoder(new FileInputStream("C:/Users/jeffr/Documents/Code/audio-poc/src/main/resources/stay.wav"));
             //MP3DecoderModded mp3Decoder = new MP3DecoderModded(new FileInputStream("C:/Users/jeffr/Documents/Code/audio-poc/src/main/resources/song1.mp3"));
@@ -73,13 +81,7 @@ public class MainController implements Initializable {
             float[][] samples = new float[1024][2];
             FastFourierTransformer fft = new FastFourierTransformer(DftNormalization.STANDARD);
 
-            ExponentialMovingAverageFilter ema0 = new ExponentialMovingAverageFilter(10);
-            ExponentialMovingAverageFilter ema1 = new ExponentialMovingAverageFilter(10);
-            ExponentialMovingAverageFilter ema2 = new ExponentialMovingAverageFilter(10);
-            ExponentialMovingAverageFilter ema3 = new ExponentialMovingAverageFilter(10);
-            ExponentialMovingAverageFilter ema4 = new ExponentialMovingAverageFilter(10);
-
-            int increment = 256 / 5;
+            int increment = 256 / numBars;
             while (mp3Decoder.readSamples( samples ) > 0) {
 
                 double[] samples_left = new double[samples.length];
@@ -92,32 +94,18 @@ public class MainController implements Initializable {
                 Complex[] fft_left = fft.transform(samples_left, TransformType.FORWARD);
                 Complex[] fft_right = fft.transform(samples_right, TransformType.FORWARD);
 
-
-                double val0 = 0;
-                for (int i = 0; i < increment; i++) {
-                    val0 += fft_left[i].abs() / 20;
+                int index = 0;
+                double val;
+                for (Node rect: visualizer.getChildren()) {
+                    val = 0;
+                    for (int i = (index * increment); i < ((index + 1) * increment); i++) {
+                        //val += fft_left[i].abs() / (float)(numBars / (index + 1));
+                        val += fft_left[i].abs() * 4;
+                    }
+                    val = val / increment;
+                    rect.setScaleY(maFilter.get(index).addAndCalculate(val));
+                    index++;
                 }
-                val0 = val0 / increment;
-                double val1 = 0;
-                for (int i = increment; i < increment * 2; i++) {
-                    val1 += fft_left[i].abs() / 10;
-                }
-                val1 = val1 / increment;
-                double val2 = 0;
-                for (int i = increment * 2; i < increment * 3; i++) {
-                    val2 += fft_left[i].abs() / 5;
-                }
-                val2 = val2 / increment;
-                double val3 = 0;
-                for (int i = increment * 3; i < increment * 4; i++) {
-                    val3 += fft_left[i].abs() / 2.5;
-                }
-                val3 = val3 / increment;
-                double val4 = 0;
-                for (int i = increment * 4; i < 256; i++) {
-                    val4 += fft_left[i].abs() / 1.25;
-                }
-                val4 = val4 / increment;
 
 //                double factor = 10;
 //                for (int i = 1000; i < 1024; i++) {
@@ -125,23 +113,6 @@ public class MainController implements Initializable {
 //                    fft_right[i] = fft_right[i].divide(factor);
 //                }
 
-                bar0.setScaleY(ema0.addAndCalculate(val0));
-                bar1.setScaleY(ema1.addAndCalculate(val1));
-                bar2.setScaleY(ema2.addAndCalculate(val2));
-                bar3.setScaleY(ema3.addAndCalculate(val3));
-                bar4.setScaleY(ema4.addAndCalculate(val4));
-
-
-//                lo = emaLo.addAndCalculate(fft_left[50].abs()/10);
-//                mid = emaMid.addAndCalculate(fft_left[500].abs()/10);
-//                hi = emaHi.addAndCalculate(fft_left[1000].abs()/10);
-//
-//                visualizerLow.setScaleX(lo);
-//                visualizerLow.setScaleY(lo);
-//                visualizerMid.setScaleX(mid);
-//                visualizerMid.setScaleY(mid);
-//                visualizerHigh.setScaleX(hi);
-//                visualizerHigh.setScaleY(hi);
 
 //                double factorLo = Math.pow(10, 0.5);
 //                for (int i = 0; i < 100; i++) {
@@ -160,5 +131,4 @@ public class MainController implements Initializable {
             e.printStackTrace();
         }
     }
-
 }
